4ec2191422b6481bf4f3b7496672a026e0f86a23
sho19 <areeb2132@gmail.com>
2022-05-28 17:09:07 +0530

    removed component rerenders

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,56 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 
 function Calculator() {
-    const [currentSum, setCurrentSum] = useState(0);
-    const [clear, setClear] = useState(false);
-    const [input, setInput] = useState();
-
-    useEffect(() => {
-        document.querySelector('#result').value = "";
-    }, [])
-
-    useEffect(() => {
-        if (clear)
-            document.querySelector('#result').value = "";
-    })
+    const [ currentSum, setCurrentSum] = useState(0);
+    const [input, setInput] = useState('');
 
     const Add = (e) => {
         e.preventDefault();
-        if (clear) setClear(false);
-        let currentNum = document.querySelector('#num').value
-        if (currentNum == '')
+        if (input == '')
             return;
-        let sum = currentSum + currentNum;
+        let sum = Number(currentSum) + Number(input);
         setCurrentSum(sum);
-        document.querySelector('#num').value = "";
-
-    }
-
-
-    const subract = (e) => {
-        e.preventDefault();
-        if (clear) setClear(false);
-        let currentNum = document.querySelector('#num').value
-        if (currentNum == '')
-            return;
-        let sum = currentSum - currentNum;
-        setCurrentSum(sum);
-        document.querySelector('#num').value = "";
-
     }
 
     const Clear = (e) => {
         e.preventDefault();
-        console.log('sum:', currentSum);
-        document.querySelector('form').reset();
-        setClear(true);
-        setCurrentSum(0);
+        setInput('')
+        setCurrentSum(0)
     }
 
     const checkForInput = (evt) => {
         evt.preventDefault();
-        if((input==0 && evt.target.value==0)){
+        if((input==Number(0) && Number(evt.target.value)==Number(0))){
             console.log("0 is being typed multiple times")
         }
         else{
@@ -59,14 +30,13 @@ function Calculator() {
 
     }
 
-
     return (
         <div className="App">
             <div className="app-title">
                 <h1> Basic Form Calculator</h1>
             </div>
             <form>
-                <input type="text" id="result" value={currentSum} readOnly />
+                <input type="text" id="result" placeholder="result" value={currentSum} readOnly />
                 <input type="number" id="num" placeholder="enter a number" onChange={(e) => { checkForInput(e) }} value={input} />
                 <button onClick={Add}>Add</button>
                 <button onClick={Clear}>Clear</button>
